770bd26282b0d08c52ed5e6b86e8af986a6ef6ad
Vitalii Lototskyi <loto9308@gmail.com>
2020-06-18 02:00:40 +0300

    #2

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS restaurants;
-CREATE TABLE restaurants (
-    id SERIAL primary key,
-    name varchar(250) not null);
-Insert into restaurants (id, name) values(1,'first lviv coronovirusarnia');
